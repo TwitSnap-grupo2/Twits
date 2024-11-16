4ddff3b327ec58552daf69e883ddab9071e0cb0b
Juani <jperezdichiazza@gmail.com>
2024-11-16 11:44:05 -0300

    feat: ping endpoint

--- src/controllers/twits.ts
+++ src/controllers/twits.ts
@@ -261,4 +261,9 @@ router.get("/stats/:id", async (req, res, next) => {
   }
 }) 
 
+router.post("/ping", async (req, res) => {
+  res.status(200).json({ message: "pong" });
+});
+
+
 export default router;
